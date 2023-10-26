c830f14bd39205a8da10b8b7dc5d3119b33550e2
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-10-26 21:56:01 +1100

    update

---
+++ app/(root)/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+// import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import { Modal } from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal'
+import { useEffect } from 'react'
+
+
+export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+ 
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+
+  return (
+   <div className='p-4'>
+    {/* <Button size="default" variant="default">Click Me</Button> */}
+    {/* <UserButton afterSignOutUrl='/'/> */}
+    {/* <Modal title="Test" description="Test Description" isOpen onClose={() => {}}>
+      Children
+    </Modal> */}
+    Root Page
+
+   </div>
+  )
+}
+
